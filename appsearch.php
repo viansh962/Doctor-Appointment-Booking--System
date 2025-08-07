b31fe3a5619851222a6d2f3cc0716f7629e7cf54
MD ANAS <itsanshsts@gmail.com>
2025-08-07 21:11:44 +0530

    Add files via upload

---
+++ appsearch.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Recipe Website</title>
+    <style>
+        body { font-family: Arial, sans-serif;}
+        header { background-color: #333; }
+        nav {background-color: #666;}
+        main {max-width: 800px; }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1> Recipe Website</h1>
+    </header>
+    <nav>
+        <a href="#breakfast">Breakfast</a>
+    </nav>
+
+    <main>
+        <section id="breakfast">
+            <h2>Breakfast Recipes</h2>
+        </section>
+    </main>
+</body>
+</html>
